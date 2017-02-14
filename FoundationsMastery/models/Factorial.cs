a2eabad04e47942a730ee82efa1fcbb7a9e2d4a7
Taylor Shuff <tkylesh@gmail.com>
2017-02-14 09:24:53 -0600

    working build, fixed 8 errors.

--- FoundationsMastery/models/Factorial.cs
+++ FoundationsMastery/models/Factorial.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FoundationsMastery.Interfaces;
 
 namespace FoundationsMastery.models
 {
-    class Factorial
+    class Factorial : IFactorial
     {
+        public int Iterative(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Recursive(int n)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
