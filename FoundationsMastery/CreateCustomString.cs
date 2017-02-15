944a10e3491823cd829f065fe194ebce3081485e
Taylor Shuff <tkylesh@gmail.com>
2017-02-15 11:46:04 -0600

    Added two new classes.

---
+++ FoundationsMastery/CreateCustomString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoundationsMastery.models;
+
+namespace FoundationsMastery
+{
+    class CreateCustomString : CustomString
+    {
+        public CreateCustomString( char[] args) : base(args)
+        {
+
+        }
+
+        
+
+    }
+}
